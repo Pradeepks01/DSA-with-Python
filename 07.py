cbbf7176bbeb66a839f9d12223ea9bb74ef17c26
Pradeepks01 <pradeepks7483@gmail.com>
2026-08-25 22:32:42 +0530

    Reverse a number

---
+++ 07.py
@@ -0,0 +1,12 @@
+class Solution:
+    # Function to reverse digits of a number
+    def reverseNumber(self, n):
+        reve_num = 0
+        while n > 0:
+            lastdigit = n % 10
+            reve_num =  reve_num * 10 + lastdigit
+            n = n // 10
+        return reve_num
+x = Solution()
+num = 12345
+print(x.reverseNumber(num))
