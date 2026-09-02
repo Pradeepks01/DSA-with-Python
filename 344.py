0fca54643bee323549f928b3105ac12a2f882f3e
Pradeepks01 <pradeepks7483@gmail.com>
2026-09-02 22:33:57 +0530

    Reverse String

---
+++ 344.py
@@ -0,0 +1,14 @@
+# Reverse string
+class Solution:
+    def reverseString(self,s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+obj = Solution()
+s = ["h","e","l","l","o"]
+obj.reverseString(s)
+print(s)
