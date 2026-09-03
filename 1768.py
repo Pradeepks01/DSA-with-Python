53cf10390bb9331589de0ca6dc66f24e14cd1b8c
Pradeepks01 <pradeepks7483@gmail.com>
2026-09-03 16:17:50 +0530

    mergeAlternately

---
+++ 1768.py
@@ -0,0 +1,19 @@
+# Merge Strings Alternately
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i , j = 0,0
+        result = ""
+        while i < len(word1) and j < len(word2):
+            if i < len(word1):
+                result += word1[i]
+                i += 1
+            if j < len(word2):
+                result += word2[j]
+                j += 1
+        return result
+
+
+obj = Solution()
+word1 = "abc"
+word2 = "pqr"
+print(obj.mergeAlternately(word1, word2))
