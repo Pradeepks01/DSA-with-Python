c656441f50fad8ed1521ec28b3e4f92d27d55a13
Pradeepks01 <pradeepks7483@gmail.com>
2026-09-03 18:23:25 +0530

    removeduplicates

---
+++ 026.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeDuplicates(self, arr):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        fast = slow = count = 0
+        while fast<len(arr):
+            if arr[fast]==arr[slow]:
+                fast+=1
+            else:
+                count+=1
+                slow+=1
+                arr[slow]=arr[fast]
+                fast+=1
+
+        return count+1
+
+obj = Solution()
+arr = [0,0,1,1,1,1,2,3,3]
+print(obj.removeDuplicates(arr))
